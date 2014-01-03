b53c6af5a1a98b216b0a102e04a335bf14e17fe7
Beta Ziliani <beta@mpi-sws.org>
2014-01-03 17:04:26 +0100

    ooops

--- MoreCoq.v
+++ MoreCoq.v
@@ -597,6 +597,7 @@ Theorem plus_n_n_injective : forall n m,
      n + n = m + m ->
      n = m.
 Proof.
+  elim=> [| n' IH].
 (** Hint: [plus_Sn_m] may be of help. *)
     (* FILL IN HERE *) Admitted.
 (** [] *)
